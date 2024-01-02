1c973e5cc3fe52e15b83a22d9685eeaf20587127
asska88 <asska88@gmail.com>
2024-01-02 14:14:32 +0700

    mengganti index.html ke index.php Changes to be committed: 	modified:   index.php 	modified:   view.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="index.html">Azka Blog</a>
+            <a class="navbar-brand" href="index.php">Azka Blog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="fas fa-bars"></i>
